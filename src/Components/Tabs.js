4853f88ad0c1a91b8888bb0a9cceff362c560b4c
parvejg <parvezpg321@gmail.com>
2023-11-21 23:26:26 +0530

    impro

--- src/Components/Tabs.js
+++ src/Components/Tabs.js
@@ -3,29 +3,50 @@ import "./Tabs.css";
 import { LondonContent, ParisContent, TokyoContent } from "./TabsContents";
 export const Tabs = () => {
   var [isActive, setActive] = useState(true);
-  const [color, setColor] = useState("whitesmoke");
   var [isActive2, setActive2] = useState(false);
   var [isActive3, setActive3] = useState(false);
-
+  const [currentTab, setCurrentTab] = useState("London");
+  const tabsData =
+    currentTab === "London" ? (
+      <LondonContent />
+    ) : currentTab === "Paris" ? (
+      <ParisContent />
+    ) : currentTab === "Tokyo" ? (
+      <TokyoContent />
+    ) : (
+      <LondonContent />
+    );
   return (
     <>
       <div className="tabs-wrapper">
-        <button className="tabs-links" onClick={() => setActive((e) => !e)}>
+        <button
+          className="tabs-links"
+          value={"London"}
+          onClick={(e) => setCurrentTab(e.target.value)}
+          style={{
+            background: currentTab === "London" ? "gray" : "whitesmoke",
+          }}
+        >
           London
         </button>
-        <button className="tabs-links" onClick={() => setActive2((e) => !e)}>
+        <button
+          value={"Paris"}
+          className="tabs-links"
+          onClick={(e) => setCurrentTab(e.target.value)}
+          style={{ background: currentTab === "Paris" ? "gray" : "whitesmoke" }}
+        >
           Paris
         </button>
-        <button className="tabs-links" onClick={() => setActive3((e) => !e)}>
+        <button
+          className="tabs-links"
+          value={"Tokyo"}
+          onClick={(e) => setCurrentTab(e.target.value)}
+          style={{ background: currentTab === "Tokyo" ? "gray" : "whitesmoke" }}
+        >
           Tokyo
         </button>
       </div>
-
-      <div className="tabs-links-content">
-        {isActive && <LondonContent />}
-        {isActive2 && <ParisContent />}
-        {isActive3 && <TokyoContent />}
-      </div>
+      <div className="tabs-links-content">{tabsData}</div>
     </>
   );
 };
