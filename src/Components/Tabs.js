ae7792b9ec1dd24235cf1d26917e936872a5afb1
parvejg <parvezpg321@gmail.com>
2023-11-21 20:06:13 +0530

    structure and onClick event work complete

---
+++ src/Components/Tabs.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./Tabs.css";
+import { LondonContent, ParisContent, TokyoContent } from "./TabsContents";
+export const Tabs = () => {
+  var [isActive, setActive] = useState();
+  const [color, setColor] = useState("whitesmoke");
+  var [isActive2, setActive2] = useState(false);
+  var [isActive3, setActive3] = useState(false);
+
+  return (
+    <>
+      <div className="tabs-wrapper">
+        <button className="tabs-links" onClick={() => setActive((e) => !e)}>
+          London
+        </button>
+        <button className="tabs-links" onClick={() => setActive2((e) => !e)}>
+          Paris
+        </button>
+        <button className="tabs-links" onClick={() => setActive3((e) => !e)}>
+          Tokyo
+        </button>
+      </div>
+
+      <div className="tabs-links-content">
+        {isActive && <LondonContent />}
+        {isActive2 && <ParisContent />}
+        {isActive3 && <TokyoContent />}
+      </div>
+    </>
+  );
+};
